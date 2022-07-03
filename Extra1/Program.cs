16059acb2ca23d1d17e86a4176888867d36e4606
Sergey Rukhlov <rukhlov-s@mail.ru>
2022-07-03 19:07:17 +0300

    Extra1

---
+++ Extra1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+
+
+Console.Write(" Введите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine(" Вторая цифра этого числа " + stringNumber[1]);
